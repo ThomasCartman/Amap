74ac629269e535ef59601422a738a2a55c5bad15
Thomas CARO <thomas.caro@niji.fr>
2014-06-21 16:30:18 +0200

    First COMMIT

---
+++ src/gui/Launcher.java
@@ -0,0 +1,13 @@
+package gui;
+
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Launcher.main()");
+		System.out.println("helloworld");
+	}
+
+}
